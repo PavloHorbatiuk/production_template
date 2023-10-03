2f885bb8dd98dec0b2099089aca2c2769f798f79
Pavlo Horbatiuk <pavlohorbatiuk@gmail.com>
2023-10-03 20:27:37 +0300

    add test for classNames

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,5 +1,40 @@
+import { classNames } from "./classNames";
+
 describe("classNames", () => {
-    test("test", () => {
-        expect(true).toBe(true);
+    test("with only first param", () => {
+        expect(classNames("someClass")).toBe("someClass");
+    });
+    test("with additional param", () => {
+        const expected = "someClass class1 class2";
+        expect(classNames("someClass", {}, ["class1", "class2"])).toBe(
+            expected
+        );
+    });
+    test("with mods", () => {
+        const expected = "someClass class1 class2 scrollAble hovered";
+        expect(
+            classNames("someClass", { scrollAble: true, hovered: true }, [
+                "class1",
+                "class2",
+            ])
+        ).toBe(expected);
+    });
+    test("with false", () => {
+        const expected = "someClass class1 class2 hovered";
+        expect(
+            classNames("someClass", { scrollAble: false, hovered: true }, [
+                "class1",
+                "class2",
+            ])
+        ).toBe(expected);
+    });
+    test("with undefined", () => {
+        const expected = "someClass class1 class2";
+        expect(
+            classNames("someClass", { scrollAble: false, hovered: undefined }, [
+                "class1",
+                "class2",
+            ])
+        ).toBe(expected);
     });
 });

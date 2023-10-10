1b033d79446db0b29d70f7c3c3a1d5aac20916c7
Pavlo Horbatiuk <pavlohorbatiuk@gmail.com>
2023-10-10 13:11:05 +0300

    added provider for testi libary react

--- src/widgets/SideBar/ui/Sidebar/SideBar.test.tsx
+++ src/widgets/SideBar/ui/Sidebar/SideBar.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { SideBar } from "./SideBar";
+import componentRender from "shared/config/tests/componentRender/componentRender";
 
 describe("Sidebar", () => {
     test("render sidebar", () => {
-        render(<SideBar />);
-        expect(screen.getByTestId("sidebar")).toBeInTheDocument();
+        componentRender(<SideBar />);
+        expect(screen.queryByTestId("sidebar")).not.toBeInTheDocument();
     });
 });

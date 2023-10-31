9c992dcabcc42418626642f0b3e202190e1cb167
Pavlo Horbatiuk <pavlohorbatiuk@gmail.com>
2023-10-31 16:12:21 +0200

    update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,37 +1,17 @@
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import { useTheme } from "app/providers/ThemeProvider";
 import { AppRouter } from "./providers/router";
 import { classNames } from "shared/lib/classNames/classNames";
 import { Navbar } from "widgets/Navbar";
 import { SideBar } from "widgets/SideBar";
-import { Modal } from "shared/ui/Modal/Modal";
 
 const App = (): JSX.Element => {
     const { theme } = useTheme();
-    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className={classNames("app", {}, [theme])}>
             <Suspense fallback=''>
                 <Navbar />
-                <button
-                    onClick={() => {
-                        setIsOpen(true);
-                    }}
-                >
-                    Modal
-                </button>
-                <Modal
-                    isOpen={isOpen}
-                    onClose={() => {
-                        setIsOpen(false);
-                    }}
-                >
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Impedit enim sint temporibus quod illum praesentium ducimus
-                    voluptas deserunt ea repellat neque quisquam accusantium
-                    laborum voluptates facere, nulla aliquid cupiditate.
-                </Modal>
                 <div className='content-page'>
                     <SideBar />
                     <AppRouter />
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,14 +1,40 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
+import { Modal } from "shared/ui/Modal/Modal";
+import { useState } from "react";
+import { Button } from "shared/ui";
+import { ButtonTheme } from "shared/ui/Button/Button";
 
 interface NavbarProps {
     className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps): JSX.Element => {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <div className={classNames(cls.navbar, {}, [className ?? ""])}>
-            <div className={cls.links}>/</div>
+            <div className={cls.links}>
+                <Button
+                    theme={ButtonTheme.OUTLINE}
+                    onClick={() => {
+                        setIsOpen(true);
+                    }}
+                >
+                    Login
+                </Button>
+                <Modal
+                    isOpen={isOpen}
+                    onClose={() => {
+                        setIsOpen(false);
+                    }}
+                >
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Impedit enim sint temporibus quod illum praesentium ducimus
+                    voluptas deserunt ea repellat neque quisquam accusantium
+                    laborum voluptates facere, nulla aliquid cupiditate.
+                </Modal>
+            </div>
         </div>
     );
 };
